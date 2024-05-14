bfad68d3ea2a531f61e6ea5056f21203776a225d
luizfiliperm <luizfilipemr17@gmail.com>
2024-05-14 17:41:07 -0300

    Saving customer

--- grails-app/controllers/com/miniasaaslw/controller/customer/CustomerController.groovy
+++ grails-app/controllers/com/miniasaaslw/controller/customer/CustomerController.groovy
@@ -1,10 +1,23 @@
 package com.miniasaaslw.controller.customer
 
+import com.miniasaaslw.domain.customer.Customer
+import com.miniasaaslw.utils.adapters.customer.CustomerAdapter
+import grails.validation.ValidationException
+
+
 class CustomerController {
+
+    def customerService
 
     def index(){
     }
 
     def save() {
+        try{
+            customerService.save(new CustomerAdapter(params))
+            redirect(action: 'index')
+        } catch (ValidationException exception){
+            render exception.errors.allErrors.defaultMessage.join(",  <br>")
+        }
     }
 }
