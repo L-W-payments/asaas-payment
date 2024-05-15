5ca539dcc6de08578891f88a72d1d4275a1ec842
Wollace Buarque <wollacebuarque@outlook.com>
2024-05-15 17:15:43 -0300

    Create findById and notDeleted named queries

--- grails-app/domain/com/miniasaaslw/domain/payer/Payer.groovy
+++ grails-app/domain/com/miniasaaslw/domain/payer/Payer.groovy
@@ -3,4 +3,17 @@ package com.miniasaaslw.domain.payer
 import com.miniasaaslw.utils.entity.BasePerson
 
 class Payer extends BasePerson {
+
+    static namedQueries = {
+        findById { Map search ->
+            boolean deleted = search.containsKey("deleted") ? Boolean.valueOf(search.deleted as String) : false
+
+            eq("id", Long.valueOf(search.id as String))
+                    .eq("deleted", deleted)
+        }
+
+        notDeleted {
+            eq("deleted", false)
+        }
+    }
 }
