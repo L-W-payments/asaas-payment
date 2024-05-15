e311a5e6945dbc036d9ee6b348d4697be266584d
Wollace Buarque <wollacebuarque@outlook.com>
2024-05-15 17:17:24 -0300

    Create delete and list methods

--- grails-app/services/com/miniasaaslw/service/PayerService.groovy
+++ grails-app/services/com/miniasaaslw/service/PayerService.groovy
@@ -9,10 +9,39 @@ import grails.validation.ValidationException
 @Transactional
 class PayerService {
 
-    public Payer find(long id) {
-        Payer payer = Payer.get(id)
+    public List<Payer> list() {
+        return Payer.notDeleted.list()
+    }
 
-        if (!payer) throw new Exception("Pagador não encontrado")
+    public Payer find(long id) {
+        Payer payer = Payer.findById([id: id]).get()
+
+        if (!payer) throw new RuntimeException("Pagador não encontrado")
+
+        return payer
+    }
+
+    public void delete(long id) {
+        Payer payer = find(id)
+
+        if (payer.deleted) {
+            throw new RuntimeException("O pagador já foi deletado")
+        }
+
+        payer.deleted = true
+
+        payer.save(failOnError: true)
+    }
+
+    public Payer update(long id, PayerAdapter payerAdapter) {
+        Payer payerValues = validatePayerParams(payerAdapter)
+
+        if (payerValues.hasErrors()) {
+            throw new ValidationException("Erro ao validar os parâmetros do pagador", payerValues.errors)
+        }
+
+        Payer payer = definePayerProperties(find(id), payerAdapter)
+        payer.save(failOnError: true)
 
         return payer
     }
@@ -24,23 +53,10 @@ class PayerService {
             throw new ValidationException("Erro ao validar os parâmetros do pagador", payerValues.errors)
         }
 
-        Payer payer = new Payer(
-                name: payerAdapter.name,
-                email: payerAdapter.email,
-                phone: payerAdapter.phone,
-                cpfCnpj: payerAdapter.cpfCnpj,
-                personType: payerAdapter.personType,
-                cep: payerAdapter.cep,
-                number: payerAdapter.number,
-                complement: payerAdapter.complement,
-                country: payerAdapter.country,
-                city: payerAdapter.city,
-                state: payerAdapter.state,
-                district: payerAdapter.district,
-                street: payerAdapter.street
-        )
+        Payer payer = definePayerProperties(new Payer(), payerAdapter)
+        payer.save(failOnError: true)
 
-        return payer.save(failOnError: true)
+        return payer
     }
 
     private Payer validatePayerParams(PayerAdapter payerAdapter) {
@@ -97,6 +113,24 @@ class PayerService {
         if (!payerAdapter.street) {
             payer.errors.reject("street", null, "A rua do pagador é obrigatória")
         }
+
+        return payer
+    }
+
+    private Payer definePayerProperties(Payer payer, PayerAdapter payerAdapter) {
+        payer.name = payerAdapter.name
+        payer.email = payerAdapter.email
+        payer.phone = payerAdapter.phone
+        payer.cpfCnpj = payerAdapter.cpfCnpj
+        payer.personType = payerAdapter.personType
+        payer.cep = payerAdapter.cep
+        payer.number = payerAdapter.number
+        payer.complement = payerAdapter.complement
+        payer.country = payerAdapter.country
+        payer.city = payerAdapter.city
+        payer.state = payerAdapter.state
+        payer.district = payerAdapter.district
+        payer.street = payerAdapter.street
 
         return payer
     }
