9fb11f1cb7136168a694f283f5f72f44f6ae96be
Wollace Buarque <wollacebuarque@outlook.com>
2024-05-15 17:16:29 -0300

    Create update endpoint, readjust redirects and show errors

--- grails-app/controllers/com/miniasaaslw/controller/payer/PayerController.groovy
+++ grails-app/controllers/com/miniasaaslw/controller/payer/PayerController.groovy
@@ -2,36 +2,63 @@ package com.miniasaaslw.controller.payer
 
 import com.miniasaaslw.domain.payer.Payer
 import com.miniasaaslw.utils.adapters.payer.PayerAdapter
+import grails.validation.ValidationException
 
 class PayerController {
 
     def payerService
 
-    def index() {}
+    def index() {
+        def errors = flash.errors
+
+        if (errors) {
+            return [errors: errors]
+        }
+    }
+
+    def update() {
+        long id = params.long("id")
+
+        try {
+            Payer payer = payerService.update(id, new PayerAdapter(params))
+
+            redirect(action: 'show', params: [id: payer.id])
+        } catch (ValidationException exception) {
+            flash.errors = exception.errors.allErrors.collect { it.defaultMessage }
+            redirect(action: 'show', params: [id: id])
+        }
+    }
 
     def save() {
         try {
             Payer payer = payerService.save(new PayerAdapter(params))
 
-            println("Pagador salvo com sucesso!")
-
             redirect(action: 'show', params: [id: payer.id])
-        } catch (Exception exception) {
-            exception.printStackTrace()
+        } catch (ValidationException exception) {
+            flash.errors = exception.errors.allErrors.collect { it.defaultMessage }
+            redirect(uri: "/payer")
         }
-
     }
 
     def show() {
         try {
             long id = params.long("id")
-            Payer payer = payerService.find(id)
 
-            println("Pagador encontrado: ${payer.name}")
-            return [payer: payer]
-        } catch (Exception ignored) {
-            println("Pagador não encontrado")
-            redirect(uri: "/")
+            return [payer: payerService.find(id)]
+        } catch (RuntimeException ignored) {
+            redirect(uri: "/payer")
         }
+    }
+
+    def delete() {
+        try {
+            long id = params.long("id")
+
+            payerService.delete(id)
+        } catch (RuntimeException ignored) {
+            println("Pagador não encontrado")
+        }
+
+        redirect(uri: "/payer")
     }
 }
