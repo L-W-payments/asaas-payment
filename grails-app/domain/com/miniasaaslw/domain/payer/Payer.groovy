b6b8876b37e72ec16d385a2bfeca575b9b39a05d
Wollace Buarque <wollacebuarque@outlook.com>
2024-05-16 18:00:51 -0300

    Using Boolean class instead of primitive type

--- grails-app/domain/com/miniasaaslw/domain/payer/Payer.groovy
+++ grails-app/domain/com/miniasaaslw/domain/payer/Payer.groovy
@@ -1,12 +1,12 @@
 package com.miniasaaslw.domain.payer
 
-import com.miniasaaslw.utils.entity.BasePerson
+import com.miniasaaslw.entity.BasePerson
 
 class Payer extends BasePerson {
 
     static namedQueries = {
         findById { Map search ->
-            boolean deleted = search.containsKey("deleted") ? Boolean.valueOf(search.deleted as String) : false
+            Boolean deleted = search.containsKey("deleted") ? Boolean.valueOf(search.deleted as String) : false
 
             eq("id", Long.valueOf(search.id as String))
                     .eq("deleted", deleted)
