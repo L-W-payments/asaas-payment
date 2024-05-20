197c1d76fd2a69ca2a814fc06b7b5b603422222c
Wollace Buarque <wollacebuarque@outlook.com>
2024-05-20 16:41:29 -0300

    Improving code legibility

--- src/main/groovy/com/miniasaaslw/utils/StateUtils.groovy
+++ src/main/groovy/com/miniasaaslw/utils/StateUtils.groovy
@@ -3,6 +3,10 @@ package com.miniasaaslw.utils
 class StateUtils {
 
     public static boolean isStateValid(String state) {
-        return state != null && state.matches('^[a-zA-Z]{2}$')
+        if (state == null) return false
+
+        if (!state.matches('^[a-zA-Z]{2}$')) return false
+
+        return true
     }
 }
