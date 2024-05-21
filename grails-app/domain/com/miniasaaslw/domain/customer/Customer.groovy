6a5782a102439e90b6bc2b8ba76d23a6068358bc
luizfiliperm <luizfilipemr17@gmail.com>
2024-05-21 14:45:57 -0300

    Adding Bi-directional relationship between Payer and Customer

--- grails-app/domain/com/miniasaaslw/domain/customer/Customer.groovy
+++ grails-app/domain/com/miniasaaslw/domain/customer/Customer.groovy
@@ -1,5 +1,10 @@
 package com.miniasaaslw.domain.customer
 
+import com.miniasaaslw.domain.payer.Payer
 import com.miniasaaslw.entity.BasePerson
 
-class Customer extends BasePerson {}
+class Customer extends BasePerson {
+
+    static hasMany = [payers: Payer]
+
+}
