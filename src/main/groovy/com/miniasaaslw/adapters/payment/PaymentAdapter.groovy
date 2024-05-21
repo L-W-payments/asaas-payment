66131ec2606a890a531bbd06666391ee066e639b
luizfiliperm <luizfilipemr17@gmail.com>
2024-05-21 18:41:27 -0300

    Adding Payment Adapter attributes

---
+++ src/main/groovy/com/miniasaaslw/adapters/payment/PaymentAdapter.groovy
@@ -0,0 +1,18 @@
+package com.miniasaaslw.adapters.payment
+
+import com.miniasaaslw.entity.enums.payment.PaymentStatus
+import com.miniasaaslw.entity.enums.payment.PaymentType
+
+class PaymentAdapter {
+
+    PaymentType paymentType
+
+    PaymentStatus paymentStatus
+
+    String description
+
+    BigDecimal value
+
+    Date dueDate
+
+}
