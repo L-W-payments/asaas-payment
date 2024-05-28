15356583da4ea3f431f89ec4c232688a545d69a4
Wollace Buarque <wollacebuarque@outlook.com>
2024-05-28 16:55:42 -0300

    Remove blank space

--- src/main/groovy/com/miniasaaslw/utils/MessageUtils.groovy
+++ src/main/groovy/com/miniasaaslw/utils/MessageUtils.groovy
@@ -4,7 +4,6 @@ import grails.util.Holders
 
 class MessageUtils {
 
-
     public static String getMessage(String code, List<String> args = []) {
         return Holders.applicationContext.getBean("messageSource").getMessage(code, args as Object[], new Locale("pt", "BR"))
     }
