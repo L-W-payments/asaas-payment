17ab169573ca92bd26e5dd6d6c8aa73ed3ebca77
luizfiliperm <luizfilipemr17@gmail.com>
2024-05-29 18:15:08 -0300

    Adding String date format

--- src/main/groovy/com/miniasaaslw/utils/DateUtils.groovy
+++ src/main/groovy/com/miniasaaslw/utils/DateUtils.groovy
@@ -3,7 +3,10 @@ package com.miniasaaslw.utils
 import java.text.SimpleDateFormat
 
 class DateUtils {
-    private static SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy")
+
+    public static final String DATE_FORMAT = "dd/MM/yyyy"
+
+    private static SimpleDateFormat inputFormat = new SimpleDateFormat(DATE_FORMAT)
 
     public static Date parseDate(String date){
         return inputFormat.parse(date)
