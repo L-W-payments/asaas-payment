d2c36d5e1feac549e1752a5e7aad24bf778324a3
luizfiliperm <luizfilipemr17@gmail.com>
2024-05-29 18:34:32 -0300

    Refactoring date util

--- src/main/groovy/com/miniasaaslw/utils/DateUtils.groovy
+++ src/main/groovy/com/miniasaaslw/utils/DateUtils.groovy
@@ -6,9 +6,7 @@ class DateUtils {
 
     public static final String DATE_FORMAT = "dd/MM/yyyy"
 
-    private static SimpleDateFormat inputFormat = new SimpleDateFormat(DATE_FORMAT)
-
     public static Date parseDate(String date){
-        return inputFormat.parse(date)
+        return new SimpleDateFormat(DATE_FORMAT).parse(date)
     }
 }
