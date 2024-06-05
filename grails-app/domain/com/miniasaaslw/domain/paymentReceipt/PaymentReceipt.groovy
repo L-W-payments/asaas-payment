c3a49390bb5d27eff88d7c73b319324556a4e493
luizfiliperm <luizfilipemr17@gmail.com>
2024-06-05 15:55:46 -0300

    Adding PaymentReceipt Domain

---
+++ grails-app/domain/com/miniasaaslw/domain/paymentReceipt/PaymentReceipt.groovy
@@ -0,0 +1,10 @@
+package com.miniasaaslw.domain.paymentReceipt
+
+import com.miniasaaslw.domain.payment.Payment
+import com.miniasaaslw.entity.BaseEntity
+
+class PaymentReceipt extends BaseEntity {
+
+    Payment payment
+
+}
