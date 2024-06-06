e239ff8abd3ac9c47a9bce3b88accdeef057bc95
luizfiliperm <luizfilipemr17@gmail.com>
2024-06-06 19:43:40 -0300

    Adding processEmailNotification and updateToSent methods

--- grails-app/services/com/miniasaaslw/service/emailnotification/EmailNotificationService.groovy
+++ grails-app/services/com/miniasaaslw/service/emailnotification/EmailNotificationService.groovy
@@ -1,11 +1,15 @@
 package com.miniasaaslw.service.emailnotification
 
 import com.miniasaaslw.adapters.emailnotification.EmailNotificationAdapter
-import com.miniasaaslw.domain.emailNotification.EmailNotification
+import com.miniasaaslw.domain.emailnotification.EmailNotification
+import com.miniasaaslw.repository.emailnotification.EmailNotificationRepository
+
 import grails.gorm.transactions.Transactional
 
 @Transactional
 class EmailNotificationService {
+
+    def mailService
 
     public void save(EmailNotificationAdapter emailNotificationAdapter) {
         EmailNotification emailNotification = new EmailNotification()
@@ -17,5 +21,37 @@ class EmailNotificationService {
         emailNotification.sent = emailNotificationAdapter.isSent
 
         emailNotification.save(failOnError: true)
+    }
+
+    public void processEmailNotification() {
+        List<Long> notSentEmailList = EmailNotificationRepository.query([:]).column("id").list(max: 500) as List<Long>
+
+        for (Long emailNotificationId : notSentEmailList) {
+            EmailNotification.withNewTransaction { status ->
+                try {
+                    sendEmail(emailNotificationId)
+                } catch (Exception exception) {
+                    log.error("Erro ao enviar e-mail de notificação de pagamento: ${exception.message}")
+                    status.setRollbackOnly()
+                }
+            }
+        }
+    }
+
+    private void updateToSent(EmailNotification emailNotification) {
+        emailNotification.sent = true
+        emailNotification.save(failOnError: true)
+    }
+
+    private void sendEmail(long emailNotificationId) {
+        EmailNotification emailNotification = EmailNotificationRepository.query([id: emailNotificationId]).get()
+
+        mailService.sendMail {
+            to emailNotification.recipientEmail
+            subject emailNotification.subject
+            text emailNotification.body
+        }
+
+        updateToSent(emailNotification)
     }
 }
