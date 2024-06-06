e9a4f0beed96c7763cdee37d51790276645ed507
Wollace Buarque <wollacebuarque@outlook.com>
2024-06-06 17:04:07 -0300

    Fix broken name validation for accents

--- src/main/groovy/com/miniasaaslw/utils/StringUtils.groovy
+++ src/main/groovy/com/miniasaaslw/utils/StringUtils.groovy
@@ -1,27 +1,37 @@
-package com.miniasaaslw.utils;
+package com.miniasaaslw.utils
+
+import java.text.Normalizer
 
 class StringUtils {
 
-    public static String removeNonNumeric(String source) {
-        if (source == null) return null;
+    public static String removeAccents(String source) {
+        if (source == null) return null
 
-        return source.replaceAll("[^0-9]", "");
+        String nfdNormalizedString = Normalizer.normalize(source, Normalizer.Form.NFD)
+
+        return nfdNormalizedString.replaceAll("[^\\p{ASCII}]", "")
+    }
+
+    public static String removeNonNumeric(String source) {
+        if (source == null) return null
+
+        return source.replaceAll("[^0-9]", "")
     }
 
     public static String camelCaseToSnakeCase(String source) {
-        if (source == null) return null;
+        if (source == null) return null
 
-        StringBuilder result = new StringBuilder();
-        char[] charArray = source.toCharArray();
+        StringBuilder result = new StringBuilder()
+        char[] charArray = source.toCharArray()
 
         for (char c : charArray) {
             if (Character.isUpperCase(c)) {
-                result.append('_').append(Character.toLowerCase(c));
+                result.append('_').append(Character.toLowerCase(c))
             } else {
-                result.append(c);
+                result.append(c)
             }
         }
 
-        return result.toString();
+        return result.toString()
     }
 }
