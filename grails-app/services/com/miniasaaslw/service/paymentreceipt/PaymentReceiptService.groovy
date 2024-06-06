77a61d24a6d9743f3f5e844744b168588b279fb0
Wollace Buarque <wollacebuarque@outlook.com>
2024-06-06 18:50:53 -0300

    Add save payment receipt endpoint

---
+++ grails-app/services/com/miniasaaslw/service/paymentreceipt/PaymentReceiptService.groovy
@@ -0,0 +1,25 @@
+package com.miniasaaslw.service.paymentreceipt
+
+import com.miniasaaslw.domain.payment.Payment
+import com.miniasaaslw.domain.paymentreceipt.PaymentReceipt
+import com.miniasaaslw.utils.MessageUtils
+
+class PaymentReceiptService {
+
+    public void save(Payment payment) {
+        if (!payment) throw new RuntimeException(MessageUtils.getMessage("payment.errors.notFound"))
+
+        if (payment.deleted) throw new RuntimeException(MessageUtils.getMessage("payment.errors.notFound"))
+
+        if (!payment.paymentStatus.isReceived() && !payment.paymentStatus.isReceivedInCash()) {
+            throw new RuntimeException(MessageUtils.getMessage("paymentReceipt.errors.notReceived"))
+        }
+
+        PaymentReceipt paymentReceipt = new PaymentReceipt()
+
+        paymentReceipt.publicId = UUID.randomUUID().toString().toUpperCase()
+        paymentReceipt.payment = payment
+
+        paymentReceipt.save(failOnError: true)
+    }
+}
