f5e468da5a9a36ef6b087b1860c5e74cd7fe00a7
luizfiliperm <luizfilipemr17@gmail.com>
2024-06-06 17:24:18 -0300

    Creating "save" EmailNotificationMethod

---
+++ grails-app/services/com/miniasaaslw/service/emailNotification/EmailNotificationService.groovy
@@ -0,0 +1,21 @@
+package com.miniasaaslw.service.emailNotification
+
+import com.miniasaaslw.adapters.emailNotification.EmailNotificationAdapter
+import com.miniasaaslw.domain.emailNotification.EmailNotification
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class EmailNotificationService {
+
+    public void save(EmailNotificationAdapter emailNotificationAdapter) {
+        EmailNotification emailNotification = new EmailNotification()
+
+        emailNotification.recipientEmail = emailNotificationAdapter.recipientEmail
+        emailNotification.subject = emailNotificationAdapter.subject
+        emailNotification.url = emailNotificationAdapter.url
+        emailNotification.body = emailNotificationAdapter.body
+        emailNotification.sent = emailNotificationAdapter.isSent
+
+        emailNotification.save(failOnError: true)
+    }
+}
