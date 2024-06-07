55c5a64a75ee93448c0eeeee92407249336b94f0
Wollace Buarque <wollacebuarque@outlook.com>
2024-06-07 18:45:39 -0300

    Refactoring save receipt validation

--- grails-app/services/com/miniasaaslw/service/paymentreceipt/PaymentReceiptService.groovy
+++ grails-app/services/com/miniasaaslw/service/paymentreceipt/PaymentReceiptService.groovy
@@ -4,16 +4,16 @@ import com.miniasaaslw.domain.payment.Payment
 import com.miniasaaslw.domain.paymentreceipt.PaymentReceipt
 import com.miniasaaslw.utils.MessageUtils
 
+import grails.validation.ValidationException
+
 class PaymentReceiptService {
 
     public void save(Payment payment) {
-        if (!payment) throw new RuntimeException(MessageUtils.getMessage("payment.errors.notFound"))
+        Payment paymentData = validatePayment(payment)
 
-        if (payment.deleted) throw new RuntimeException(MessageUtils.getMessage("payment.errors.notFound"))
-
-        if (payment.paymentStatus.isReceivedInCash()) throw new RuntimeException(MessageUtils.getMessage("paymentReceipt.errors.receivedInCash"))
-
-        if (!payment.paymentStatus.isReceived()) throw new RuntimeException(MessageUtils.getMessage("paymentReceipt.errors.notReceived"))
+        if (paymentData.hasErrors()) {
+            throw new ValidationException(MessageUtils.getMessage("general.errors.validation"), paymentData.errors)
+        }
 
         PaymentReceipt paymentReceipt = new PaymentReceipt()
 
@@ -21,5 +21,27 @@ class PaymentReceiptService {
         paymentReceipt.payment = payment
 
         paymentReceipt.save(failOnError: true)
+    }
+
+    public Payment validatePayment(Payment payment) {
+        Payment validatePayment = new Payment()
+
+        if (!payment) {
+            validatePayment.errors.reject("payment", null, MessageUtils.getMessage("payment.errors.notFound"))
+        }
+
+        if (payment.deleted) {
+            validatePayment.errors.reject("deleted", null, MessageUtils.getMessage("payment.errors.deleted"))
+        }
+
+        if (payment.paymentStatus.isReceivedInCash()) {
+            validatePayment.errors.reject("paymentStatus", null, MessageUtils.getMessage("payment.errors.receivedInCash"))
+        }
+
+        if (!payment.paymentStatus.isReceived()) {
+            validatePayment.errors.reject("paymentStatus", null, MessageUtils.getMessage("payment.errors.notReceived"))
+        }
+
+        return validatePayment
     }
 }
