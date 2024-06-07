8460eeefd576603d726b13389f79f64c44d93955
luizfiliperm <luizfilipemr17@gmail.com>
2024-06-07 15:30:57 -0300

    Refactor EmailNotificationJob - logError and trigger

--- grails-app/jobs/com/miniasaaslw/jobs/ProcessEmailNotificationJob.groovy
+++ grails-app/jobs/com/miniasaaslw/jobs/ProcessEmailNotificationJob.groovy
@@ -1,18 +1,20 @@
 package com.miniasaaslw.jobs
 
+import java.time.Duration
+
 class ProcessEmailNotificationJob {
 
     def emailNotificationService
 
     static triggers = {
-        simple name: "processEmailNotificationJobTrigger", repeatInterval: 1000 * 60
+        simple name: "processEmailNotificationJobTrigger", repeatInterval: Duration.ofSeconds(60).toMillis()
     }
 
     def execute() {
         try {
             emailNotificationService.processEmailNotification()
         } catch (Exception exception) {
-            log.error("Erro ao tentar enviar o email", exception)
+            log.error("ProcessEmailNotificationJob.execute >> Erro ao tentar enviar o email", exception)
         }
     }
 }
