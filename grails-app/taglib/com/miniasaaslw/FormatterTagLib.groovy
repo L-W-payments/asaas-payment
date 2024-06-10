39d0f1f33dc401a50d552c682803cc3ea69e7edf
luizfiliperm <luizfilipemr17@gmail.com>
2024-06-10 18:37:22 -0300

    Adding cpf formater

--- grails-app/taglib/com/miniasaaslw/FormatterTagLib.groovy
+++ grails-app/taglib/com/miniasaaslw/FormatterTagLib.groovy
@@ -9,4 +9,10 @@ class FormatterTagLib {
         String format = DateUtils.SIMPLE_DATE_FORMAT
         out << g.formatDate(format: format, date: attrs.date)
     }
+
+    def cpf = { attrs ->
+        String cpf = attrs.cpf
+        out << cpf[0..2] + "." + cpf[3..5] + "." + cpf[6..8] + "-" + cpf[9..10]
+    }
+
 }
