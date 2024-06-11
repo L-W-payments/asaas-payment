4f76b1f1926019eb9be27127536c098d16c81164
Wollace Buarque <wollacebuarque@outlook.com>
2024-06-11 17:08:44 -0300

    Remove pagination from notification

--- grails-app/controllers/com/miniasaaslw/controller/notification/NotificationController.groovy
+++ grails-app/controllers/com/miniasaaslw/controller/notification/NotificationController.groovy
@@ -6,7 +6,7 @@ import com.miniasaaslw.utils.LoggedCustomer
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 
-@Secured(['ROLE_MEMBER'])
+@Secured(['isAuthenticated()'])
 class NotificationController {
 
     def notificationService
@@ -15,7 +15,7 @@ class NotificationController {
 
     def list() {
         try {
-            List<Notification> notifications = notificationService.list(LoggedCustomer.CUSTOMER.id, 50, 0)
+            List<Notification> notifications = notificationService.list(LoggedCustomer.CUSTOMER.id)
             String template = g.render(template: "/templates/notifications", model: [notifications: notifications])
 
             render([success: true, template: template] as JSON)
