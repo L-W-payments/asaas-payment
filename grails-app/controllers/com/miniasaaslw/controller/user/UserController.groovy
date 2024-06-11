7072714d95aa22aa226c0bbbb6f302d42e953e07
luizfiliperm <luizfilipemr17@gmail.com>
2024-06-11 19:21:56 -0300

    Adding show endpoint

--- grails-app/controllers/com/miniasaaslw/controller/user/UserController.groovy
+++ grails-app/controllers/com/miniasaaslw/controller/user/UserController.groovy
@@ -29,6 +29,35 @@ class UserController extends BaseController {
         }
     }
 
+    @CompileDynamic
+    @Secured(['isAuthenticated()'])
+    def show() {
+        def messageInfo = flash.messageInfo
+
+        if (messageInfo) {
+            return [messageInfo: messageInfo]
+        }
+
+        try {
+            User loggedUser = getAuthenticatedUser() as User
+            Boolean isAdmin = loggedUser.getAuthorities().stream().any { it.authority == 'ROLE_ADMIN' }
+
+            if (isAdmin) {
+                if(params.id){
+                    return [user: userService.find((getAuthenticatedUser() as User).customerId, params.long("id"))]
+                }
+            }
+
+            return [user: (getAuthenticatedUser() as User)]
+        } catch (RuntimeException runtimeException) {
+            flash.messageInfo = [messages: [runtimeException.getMessage()], messageType: "error"]
+        } catch (Exception exception) {
+            flash.messageInfo = [messages: [MessageUtils.getMessage("user.errors.save.unknown")], messageType: "error"]
+        }
+
+        redirect(action: "index")
+    }
+
     def save() {
         try {
             userService.save(new UserAdapter((getAuthenticatedUser() as User).customer, Role.findByAuthority('ROLE_MEMBER'), params))
