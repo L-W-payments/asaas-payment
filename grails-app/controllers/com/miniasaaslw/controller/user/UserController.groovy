5ceaece47b7a7f47fbebb4583b57974aabee05f3
Wollace Buarque <wollacebuarque@outlook.com>
2024-06-12 19:55:10 -0300

    Add restore to user

--- grails-app/controllers/com/miniasaaslw/controller/user/UserController.groovy
+++ grails-app/controllers/com/miniasaaslw/controller/user/UserController.groovy
@@ -82,6 +82,24 @@ class UserController extends BaseController {
         }
     }
 
+    @Secured(['ROLE_ADMIN'])
+    def restore() {
+        try {
+            Long id = params.long("id")
+
+            User loggedUser = getAuthenticatedUser() as User
+
+            userService.restore(loggedUser.customer.id, id)
+            render([success: true] as JSON)
+        } catch (RuntimeException runtimeException) {
+            flash.messageInfo = [messages: [runtimeException.getMessage()], messageType: "error"]
+            render([success: false, alert: runtimeException.getMessage()] as JSON)
+        } catch (Exception exception) {
+            flash.messageInfo = [messages: [MessageUtils.getMessage("user.errors.restore.unknown")], messageType: "error"]
+            render([success: false, alert: MessageUtils.getMessage("user.errors.restore.unknown")] as JSON)
+        }
+    }
+
     def list() {
         return [userList: userService.list([customerId: (getAuthenticatedUser() as User).customerId], getLimitPerPage(), getOffset())]
     }
