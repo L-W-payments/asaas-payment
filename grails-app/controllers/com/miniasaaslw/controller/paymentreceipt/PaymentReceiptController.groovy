55af508fe1bd964c534b360a1f91aad411b9d7a7
luizfiliperm <luizfilipemr17@gmail.com>
2024-06-12 16:49:37 -0300

    Treating message errors

--- grails-app/controllers/com/miniasaaslw/controller/paymentreceipt/PaymentReceiptController.groovy
+++ grails-app/controllers/com/miniasaaslw/controller/paymentreceipt/PaymentReceiptController.groovy
@@ -1,18 +1,27 @@
 package com.miniasaaslw.controller.paymentreceipt
 
+import com.miniasaaslw.service.paymentreceipt.PaymentReceiptService
+import com.miniasaaslw.utils.MessageUtils
+import grails.compiler.GrailsCompileStatic
+import grails.plugin.springsecurity.annotation.Secured
+
+@GrailsCompileStatic
+@Secured(["permitAll"])
 class PaymentReceiptController {
 
-    def paymentReceiptService
+    PaymentReceiptService paymentReceiptService
 
     def index() {}
 
     def show() {
         try {
             String publicId = params.id
-
+            throw new Exception("Sla")
             return [receipt: paymentReceiptService.find(publicId)]
+        } catch (RuntimeException runtimeException) {
+            return [message: MessageUtils.getMessage("paymentReceipt.errors.notFound"), messageType: "notFound"]
         } catch (Exception exception) {
-            redirect(uri: "/")
+            return [message: MessageUtils.getMessage("paymentReceipt.errors.show.unknown"), messageType: "unknown"]
         }
     }
 }
