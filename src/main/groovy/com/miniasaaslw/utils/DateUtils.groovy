f747072f0724b2cd3331718f3871b952d3e362d3
luizfiliperm <luizfilipemr17@gmail.com>
2024-06-13 17:04:07 -0300

    Removing DateUtils "today"

--- src/main/groovy/com/miniasaaslw/utils/DateUtils.groovy
+++ src/main/groovy/com/miniasaaslw/utils/DateUtils.groovy
@@ -26,13 +26,4 @@ class DateUtils {
         calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH))
         return calendar.getTime()
     }
-
-    public static Date today() {
-        Calendar calendar = Calendar.getInstance()
-        calendar.set(Calendar.HOUR_OF_DAY, 0)
-        calendar.set(Calendar.MINUTE, 0)
-        calendar.set(Calendar.SECOND, 0)
-        calendar.set(Calendar.MILLISECOND, 0)
-        return calendar.getTime()
-    }
 }
